98f9cd19d8ddad35b512fa47805ded5e8c3c8cee
snehithreddy1682003@gmail.com <saikiran@sais-mbp.home>
2024-02-19 22:42:38 -0500

    first commit

---
+++ AppointmentConnect/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppointmentConnect;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AppointmentConnect/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppointmentConnect;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
