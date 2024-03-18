15287d2128ec30c68ccbdad2a7872c569c72c4d3
sai <saikiran20mar@gmail.com>
2024-03-18 12:27:37 -0400

    new code has being pushed

---
+++ AppointmentConnect/AppointmentPage.cs
@@ -0,0 +1,14 @@
+﻿namespace AppointmentConnect
+{
+    internal class AppointmentPage : Page
+    {
+        private object text1;
+        private object text2;
+
+        public AppointmentPage(object text1, object text2)
+        {
+            this.text1 = text1;
+            this.text2 = text2;
+        }
+    }
+}
